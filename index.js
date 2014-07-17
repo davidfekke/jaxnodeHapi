78b389ed04a87850de55e32a8338eb02f1308e9e
David Fekke <david@fekke.com>
2014-07-17 11:12:25 -0400

    Added example of server.method

--- index.js
+++ index.js
@@ -12,6 +12,37 @@ var config = {
 
 var server = new Hapi.Server('localhost', 3000, config);
 
+var myList = [
+	{ _id: 1, name: 'Bob' },
+	{ _id: 2, name: 'Tom' },
+	{ _id: 3, name: 'Kevin' },
+	{ _id: 4, name: 'Kunal' },
+	{ _id: 5, name: 'John' },
+	{ _id: 6, name: 'David' },
+	{ _id: 7, name: 'Jason' },
+	{ _id: 8, name: 'Jannaee' }
+].sort(function (a, b) {
+	if (a.name > b.name)
+		return 1;
+	if (a.name < b.name)
+		return -1;
+	// a must be equal to b
+	return 0;
+});
+
+function listGetById(aList, id, next) {
+	for (var key in aList) {
+        var curItem = aList[key];
+		if (curItem._id.toString() === id)
+		{
+			next(null, aList[key]);
+		}
+	}
+    next(null, { _id:-1, name: 'unknown'} );
+}
+
+server.method({ name: 'listGetById', fn: listGetById });
+
 var routes = [
 	{
 	    method: 'GET',
@@ -38,24 +69,16 @@ var routes = [
 		method: 'GET',
 		path: '/mobile/list',
 		handler: function (request, reply) {
-			var myList = [
-				{ _id: 1, name: 'Bob' },
-				{ _id: 2, name: 'Tom' },
-				{ _id: 3, name: 'Kevin' },
-				{ _id: 4, name: 'Kunal' },
-				{ _id: 5, name: 'John' },
-				{ _id: 6, name: 'David' },
-				{ _id: 7, name: 'Jason' },
-				{ _id: 8, name: 'Jannaee' }
-			].sort(function (a, b) {
-				if (a.name > b.name)
-					return 1;
-				if (a.name < b.name)
-				    return -1;
-				    // a must be equal to b
-				return 0;
-			});;
 			reply(myList);
+		}
+	},
+	{
+		method: 'GET',
+		path: '/mobile/list/{id}',
+		handler: function (request, reply) {
+			server.methods.listGetById(myList, request.params.id, function(err, result) {
+				reply(result);
+			});
 		}
 	},
 	{
